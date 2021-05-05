fix softmax

scenario_1/net.py
@@ -29,7 +29,7 @@ class SimpleNet(nn.Module):
         x = f.relu(self.conv5(x))
         #x = f.relu(self.conv6(x))
         #print(x.shape)
-        x = f.softmax(self.linear(x.view(x.size(0), -1)))
+        x = self.linear(x.view(x.size(0), -1))
 
         return x
 
@@ -55,6 +55,6 @@ class SimpleNet1D(nn.Module):
         #x = f.relu(self.conv5(x))
         #x = f.relu(self.conv6(x))
         #print(x.shape)
-        x = f.softmax(self.linear(x.view(x.size(0), -1)))
+        x = self.linear(x.view(x.size(0), -1))
 
         return x

scenario_1/training.py
@@ -1,6 +1,7 @@
 import torch
 from torch import optim
 from net import SimpleNet, SimpleNet1D
+import torch.nn.functional as f
 from torch import nn
 from utils import get_simple_data_loader, get_simple_eval_loader
 import matplotlib.pyplot as plt
@@ -15,11 +16,11 @@ num_epochs = 20
 device = 'cuda'
 
 # change the model here. Also change view in loading methods
-#model = SimpleNet()
-model = mobilenet_v2(pretrained=True)
+model = SimpleNet()
+# model = mobilenet_v2(pretrained=True)
 
-model.features[0][0] = nn.Conv2d(1, 32, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1), bias=False)
-model.classifier[1] = nn.Linear(in_features=model.classifier[1].in_features, out_features=55)
+# model.features[0][0] = nn.Conv2d(1, 32, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1), bias=False)
+# model.classifier[1] = nn.Linear(in_features=model.classifier[1].in_features, out_features=55)
 model.to(device)
 
 optimizer = optim.Adam(model.parameters(), lr=lr)
@@ -66,7 +67,7 @@ correct_pred = 0
 num_pred = 0
 
 for data, label in eval_loader:
-    pred = torch.argmax(model(data))
+    pred = torch.argmax(f.softmax(model(data)))
     num_pred += 1
 
     if pred.data.item() == label.data.item():