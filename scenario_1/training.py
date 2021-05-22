1d940511dad833d53af9ece93b5872ad2864eaed
Yes25 <yes.92@kielnet.net>
2021-05-22 15:55:54 +0200

    measures

--- scenario_1/training.py
+++ scenario_1/training.py
@@ -7,12 +7,14 @@ from utils import get_simple_data_loader, get_simple_eval_loader
 import matplotlib.pyplot as plt
 import os
 from meanAveragePrecision import computeMeanAveragePrecision
+from sklearn.metrics import f1_score
+import numpy as np
 
 """ 
 Inits:
 """
 lr = 0.001
-num_epochs = 1  # 50
+num_epochs = 50
 device = 'cpu'
 if torch.cuda.is_available():
     device = 'cuda'
@@ -62,22 +64,34 @@ correct_pred = 0
 num_pred = 0
 map_sum = 0
 
+label_ls = []
+pred_ls = []
+estimation_ls = []
+
 for data, label in eval_loader:
-    estimation = f.softmax(model(data))
+    estimation = f.softmax(model(data), dim=1)
     pred = torch.argmax(estimation)
     num_pred += 1
 
     if pred.data.item() == label.data.item():
         correct_pred += 1
 
-    map, ap = computeMeanAveragePrecision(label.detach().numpy(), estimation.detach().numpy())
-    map_sum += map
-    print(num_pred)
+    label_ls.append(label.data.item())
+    pred_ls.append(pred.data.item())
+    estimation_ls.append(estimation.cpu().detach().numpy())
+
+
+    # map, ap = computeMeanAveragePrecision(labels=label.cpu().detach().numpy(), softmaxEstimations=estimation.cpu().detach().numpy())
+    # map_sum += map
 
 print('Number of correct predictions: ' + str(correct_pred))
 print('Number of Predictions: ' + str(num_pred))
-print('Portion: ' + str(correct_pred/num_pred))
-print('MAP: ' + str(map_sum/num_pred))
+print('Accuracy: ' + str(correct_pred/num_pred))
+print('F1-Score: ' + str(f1_score(y_true=label_ls, y_pred=pred_ls, average='micro')))
+
+map, ap = computeMeanAveragePrecision(labels=label_ls, softmaxEstimations=np.array(estimation_ls).squeeze())
+
+print('MAP: ' + str(map))
 
 
 # Uncomment the following lines if you want the model to be saved
