23c3124f0676bf89e6f83e65a6fce1b18eed2994
Konrad <konrad.vonkuegelgen@student.uni-luebeck.de>
2021-05-25 16:44:14 +0200

    add track1

--- scenario_2/Reader.py
+++ scenario_2/Reader.py
@@ -1,7 +1,11 @@
 import pandas as pd
 import os
 
-def csv_reader(folder='dataset'):
+def csv_reader(folder='dataset/track1'):
     accelerometer = pd.read_csv(os.path.join(folder,'Accelerometer.csv'))
     gyroscope = pd.read_csv(os.path.join(folder,'Gyroscope.csv'))
     return accelerometer, gyroscope
+
+
+folder = 'dataset/track1'
+acc, gyro = csv_reader(folder=folder)
