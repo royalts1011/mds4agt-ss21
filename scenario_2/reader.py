7a3e3ee19538695e34930a7d80f97a78975df4ca
Konrad <konrad.vonkuegelgen@student.uni-luebeck.de>
2021-05-26 10:15:44 +0200

    ciao numpy

--- scenario_2/reader.py
+++ scenario_2/reader.py
@@ -1,7 +1,5 @@
 from os.path import join
 import pandas as pd
-import numpy as np
-
 
 def csv_to_dataframe(folder):
     """
@@ -11,16 +9,6 @@ def csv_to_dataframe(folder):
     # No skipped rows as the first row is the heder of the DataFrame
     accelerometer = pd.read_csv(join(folder,'Accelerometer.csv'))
     gyroscope = pd.read_csv(join(folder,'Gyroscope.csv'))
-    return accelerometer, gyroscope
-
-
-def numpy_csv_to_array(folder):
-    """
-        Very slow but direct version of converting the csv
-    """
-    # Skip first frow for clean number array
-    accelerometer = np.loadtxt(join(folder,'Accelerometer.csv'), delimiter=',', skiprows=1)
-    gyroscope = np.loadtxt(join(folder,'Gyroscope.csv'), delimiter=',', skiprows=1)
     return accelerometer, gyroscope
 
 
@@ -43,9 +31,3 @@ acc = acc.to_numpy()
 gyro = gyro.to_numpy()
 print(acc[0:5,:])
 print(gyro[0:5,:])
-
-
-# Numpy Version *SLOW*
-acc, gyro = numpy_csv_to_array(folder=track_dir)
-print (acc[0:5,:]) # first 5 rows
-print (gyro[0:5,:]) # first 5 rows
