8454689e1f968f415b811976ca4d29d37b26c3c6
royalts1011 <falco.lentzsch@gmail.com>
2021-06-01 14:55:26 +0200

    plottet acc data + low pass filter + step detection

--- scenario_2/reader.py
+++ scenario_2/reader.py
@@ -7,16 +7,16 @@ def csv_to_dataframe(folder):
         Fast pandas conversion to DataFrame.
         Return DataFrame format 
     """
-    # No skipped rows as the first row is the heder of the DataFrame
+    # No skipped rows as the first row is the header of the DataFrame
     accelerometer = pd.read_csv(join(folder,'Accelerometer.csv'))
     gyroscope = pd.read_csv(join(folder,'Gyroscope.csv'))
     return accelerometer, gyroscope
 
-
 def csv_to_array(folder):
     """
-        This function can be used from externa to translate a CSV
-        into a numpy array.
+    :param: data:
+    This function can be used from externa to translate a CSV
+    into a numpy array.
     """
     acc, gyro = csv_to_dataframe(folder)
     return acc.to_numpy(), gyro.to_numpy()
@@ -30,14 +30,3 @@ def get_sample_freq(data):
 
     return int(math.ceil(freq / 100.0)) * 100
 
-track_dir = 'dataset/track1'
-
-# Pandas version *FAST*
-acc, gyro = csv_to_dataframe(folder=track_dir)
-print(acc.head(5))
-print(gyro.head(5))
-
-acc = acc.to_numpy()
-gyro = gyro.to_numpy()
-print(acc[0:5,:])
-print(gyro[0:5,:])
