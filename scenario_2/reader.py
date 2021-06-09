a45c57042f96be03ce02ec4450a7b9d963aa63b5
royalts1011 <falco.lentzsch@gmail.com>
2021-06-09 12:06:46 +0200

    added geb 64 track + load function with delimiter now + crop_factor

--- scenario_2/reader.py
+++ scenario_2/reader.py
@@ -2,31 +2,33 @@ from os.path import join
 import pandas as pd
 import math
 
-def csv_to_dataframe(folder):
+
+def csv_to_dataframe(folder, delimiter=','):
     """
         Fast pandas conversion to DataFrame.
         Return DataFrame format 
     """
     # No skipped rows as the first row is the header of the DataFrame
-    accelerometer = pd.read_csv(join(folder,'Accelerometer.csv'))
-    gyroscope = pd.read_csv(join(folder,'Gyroscope.csv'))
+    accelerometer = pd.read_csv(join(folder, 'Accelerometer.csv'), delimiter=delimiter)
+    gyroscope = pd.read_csv(join(folder, 'Gyroscope.csv'), delimiter=delimiter)
     return accelerometer, gyroscope
 
-def csv_to_array(folder):
+
+def csv_to_array(folder, delimiter):
     """
     :param: data:
     This function can be used from externa to translate a CSV
     into a numpy array.
     """
-    acc, gyro = csv_to_dataframe(folder)
+    acc, gyro = csv_to_dataframe(folder, delimiter)
     return acc.to_numpy(), gyro.to_numpy()
+
 
 def get_sample_freq(data):
     """
          :return: rounded sample frequency in Hz
     """
-    time = data[len(data)-2][0]
-    freq = len(data) / time # if needed this is the original freq
+    time = data[len(data) - 2][0]
+    freq = len(data) / time  # if needed this is the original freq
 
     return int(math.ceil(freq / 100.0)) * 100
-
