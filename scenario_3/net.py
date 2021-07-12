238fc1f13811accbded336fbbe0af700a2f16dfc
Konrad <konrad.vonkuegelgen@student.uni-luebeck.de>
2021-07-12 11:48:02 +0200

    fin

--- scenario_3/net.py
+++ scenario_3/net.py
@@ -3,7 +3,7 @@ from torch import nn
 import torch.nn.functional as f
 
 ### Variables ###
-kern_sz = (1, 5)
+kern_sz = (1, 20)
 stride = (1, 2)
 padding = 0
 
@@ -42,8 +42,8 @@ class SimpleNet(nn.Module):
             nn.ReLU()
         )
 
-        self.linear = nn.Linear(256 * 1 * 43, 5) # bei kernel size (1,5)
-        # self.linear = nn.Linear(256 * 1 * 29, 5) # bei kernel size (1,20)
+        # self.linear = nn.Linear(256 * 1 * 43, 5) # bei kernel size (1,5)
+        self.linear = nn.Linear(256 * 1 * 29, 5) # bei kernel size (1,20)
         # self.linear = nn.Linear(256 * 1 * 10, 5) # bei kernel size (1,40)
 
 
